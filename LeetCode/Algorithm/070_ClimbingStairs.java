ed8db376112df827a07382f9dc45581b0f807569
DdMad <ddmad1030@gmail.com>
2016-04-28 04:14:06 +0800

    Add 070 solution.

---
+++ LeetCode/Algorithm/070_ClimbingStairs.java
@@ -0,0 +1,25 @@
+/*
+
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+*/
+
+public class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int[] f = new int[n + 1];
+        f[0] = 0;
+        f[1] = 1;
+        f[2] = 2;
+        
+        for (int i = 3; i <= n; i++) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+        
+        return f[n];
+    }
+}
