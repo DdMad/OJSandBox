b9cdf3ef698509a8c86ab665b51d81935ca83ca4
DdMad <ddmad1030@gmail.com>
2016-06-09 13:02:46 +0800

    Add 354 solution.

---
+++ LeetCode/Algorithm/354_RussianDollEnvelopes.java
@@ -0,0 +1,43 @@
+/*
+
+You have a number of envelopes with widths and heights given as a pair of integers (w, h). One envelope can fit into another if and only if both the width and height of one envelope is greater than the width and height of the other envelope.
+
+What is the maximum number of envelopes can you Russian doll? (put one inside other)
+
+Example:
+Given envelopes = [[5,4],[6,4],[6,7],[2,3]], the maximum number of envelopes you can Russian doll is 3 ([2,3] => [5,4] => [6,7]).
+
+*/
+
+public class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        if (envelopes == null || envelopes.length == 0) {
+            return 0;
+        }
+        
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return Integer.compare(a[0], b[0]);
+            }
+        });
+        
+        int[] f = new int[envelopes.length];
+        Arrays.fill(f, 1);
+        int max = f[0];
+        
+        // Using the same method for Longest Increasing Subsequence(300)
+        for (int i = 0; i < envelopes.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (envelopes[j][0] < envelopes[i][0] && envelopes[j][1] < envelopes[i][1]) {
+                    f[i] = Math.max(f[i], f[j] + 1);
+                }
+            }
+            
+            // Since the largest may not end with last element
+            // Need to find out largest
+            max = Math.max(max, f[i]);
+        }
+        
+        return max;
+    }
+}
